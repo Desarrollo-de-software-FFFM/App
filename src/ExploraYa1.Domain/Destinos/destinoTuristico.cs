0553e113d78a323aea241810b95803b1ac1f185b
Franc <francisco2do1994@gmail.com>
2025-09-17 20:18:53 -0300

    Definicion de clase DestinoTuristico

---
+++ src/ExploraYa1.Domain/Destinos/destinoTuristico.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.ExceptionHandling;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExploraYa1.Destinos
+{
+    public class destinoTuristico : AuditedAggregateRoot<Guid>
+    {
+        [Required]
+        [StringLength(100)]
+        public string nombre { get; set; }
+        [Required]
+        [Range(0,int.MaxValue,ErrorMessage = "La poblacion debe ser un numero positivo")]
+        public int poblacion { get; set; }
+        public float latitud { get; set; }
+        public float longuitud { get; set; }
+        public string imagenUrl { get; set; }
+        public int calificacionGeneral { get; set; }
+
+
+    }
+}
