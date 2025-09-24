699825627ca42d5220e281ccd7da54ca0ceb488c
Franc <francisco2do1994@gmail.com>
2025-09-24 19:19:02 -0300

    Agregado de validadores Clase destino

--- src/ExploraYa1.Domain/Destinos/destinoTuristico.cs
+++ src/ExploraYa1.Domain/Destinos/destinoTuristico.cs
@@ -18,9 +18,17 @@ namespace ExploraYa1.Destinos
         [Required]
         [Range(0,int.MaxValue,ErrorMessage = "La poblacion debe ser un numero positivo")]
         public int poblacion { get; set; }
+        [Required]
+        [Range(-90, 90, ErrorMessage = "La latitud debe estar entre -90 y 90.")]
         public float latitud { get; set; }
+        [Required]
+        [Range(-180, 180, ErrorMessage = "La longitud debe estar entre -180 y 180.")]
         public float longuitud { get; set; }
+        [Required]
+        [StringLength(200, ErrorMessage = "La URL de la imagen no puede superar los 200 caracteres.")]
         public string imagenUrl { get; set; }
+        [Required]
+        [Range(0, 5, ErrorMessage = "La calificación general debe estar entre 0 y 5")]
         public int calificacionGeneral { get; set; }
 
 
