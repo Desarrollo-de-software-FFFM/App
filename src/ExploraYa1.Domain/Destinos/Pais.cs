97c80580c9ce2c14e958c196ba34aab47c8d858c
Franc <francisco2do1994@gmail.com>
2025-09-24 20:01:33 -0300

    Agregado de las clases Pais y Region

---
+++ src/ExploraYa1.Domain/Destinos/Pais.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploraYa1.Destinos
+{
+    public class Pais
+    {
+        [Required] [StringLength(100)] public required string Nombre { get; set; }
+    }
+}
---
+++ src/ExploraYa1.Domain/Destinos/Region.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploraYa1.Destinos
+{
+    public class Region
+    {
+        [Required]
+        [StringLength(100)]
+        public required string Nombre { get; set; }
+        [Required]
+        [StringLength(300)]
+        public required string Descripcion { get; set; }
+        
+    }
+}
