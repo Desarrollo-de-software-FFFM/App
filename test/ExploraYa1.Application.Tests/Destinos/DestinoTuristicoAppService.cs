58ac6352f61d37376411c39c3c74843af4960337
Felipe <Felipe@FELIPE-NAVE12>
2025-10-03 18:23:24 -0300

    prueba profe

--- test/ExploraYa1.Application.Tests/Destinos/DestinoTuristicoAppService.cs
+++ test/ExploraYa1.Application.Tests/Destinos/DestinoTuristicoAppService.cs
@@ -10,58 +10,60 @@ using Volo.Abp.Modularity;
 using Xunit;
 using Volo.Abp.Testing;
 using Volo.Abp.Autofac;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Validation;
 
 
 namespace ExploraYa1.Destinos
 {
+
+
     public abstract class DestinoTuristicoAppService_tests<TStartupModule> : ExploraYa1ApplicationTestBase<TStartupModule>
         where TStartupModule : IAbpModule
 
-        
+
     {
         private readonly IDestinosAppService _destinosAppService;
-   
+
         protected DestinoTuristicoAppService_tests()
         {
             _destinosAppService = GetRequiredService<IDestinosAppService>();
         }
 
         [Fact]
-        public async Task CreateAsync_ShouldReturnCreatedDestinosDto()         
+        public async Task CreateAsync_ShouldReturnCreatedDestinosDto()
         {
             // Arrange
             var crearDestinoDTO = new CrearActualizarDestinoDTO
             {
-                Nombre = "Parque Nacional",
+                Nombre = "ParqueNacional",
                 Latitud = 34,
-                Longuitud = -118,
+                Longuitud = 40,
                 Poblacion = 500,
-                CalificacionGeneral = 5,
+                CalificacionGeneral = 4,
                 ImagenUrl = "asdasdasd",
-                RegionId = new Guid("22222222 - 2222 - 2222 - 2222 - 222222222222") // Asegúrate de usar un GUID válido
+                RegionId = Guid.NewGuid() // Asegúrate de usar un GUID válido
+
             };
-            
+
             // Act
-            var createdDestino = await _destinosAppService.CreateAsync(crearDestinoDTO);
-            
-            
-            
+            var result = await _destinosAppService.CreateAsync(crearDestinoDTO);
+
+
+
             // Assert
-            createdDestino.ShouldNotBeNull();
-            createdDestino.Id.ShouldNotBe(Guid.Empty);
-            createdDestino.Nombre.ShouldBe(crearDestinoDTO.Nombre);
-            createdDestino.Poblacion.ShouldBe(crearDestinoDTO.Poblacion);
-            createdDestino.CalificacionGeneral.ShouldBe(crearDestinoDTO.CalificacionGeneral);
-            createdDestino.ImagenUrl.ShouldBe(crearDestinoDTO.ImagenUrl);
-            createdDestino.Latitud.ShouldBe(crearDestinoDTO.Latitud);
-            createdDestino.Longuitud.ShouldBe(crearDestinoDTO.Longuitud);
-            createdDestino.RegionId.ShouldBe(crearDestinoDTO.RegionId);
+            result.ShouldNotBeNull();
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Nombre.ShouldBe(crearDestinoDTO.Nombre);
+            result.Poblacion.ShouldBe(crearDestinoDTO.Poblacion);
+            result.CalificacionGeneral.ShouldBe(crearDestinoDTO.CalificacionGeneral);
+            result.ImagenUrl.ShouldBe(crearDestinoDTO.ImagenUrl);
+            result.Latitud.ShouldBe(crearDestinoDTO.Latitud);
+            result.Longuitud.ShouldBe(crearDestinoDTO.Longuitud);
+            result.RegionId.ShouldBe(crearDestinoDTO.RegionId);
         }
 
     }
-
-
-
 
 
 
