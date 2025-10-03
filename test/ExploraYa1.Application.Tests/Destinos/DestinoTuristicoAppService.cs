932e156bc833b71726f7269198ffef7864957a45
Felipe <Felipe@FELIPE-NAVE12>
2025-10-03 14:54:04 -0300

    tests

---
+++ test/ExploraYa1.Application.Tests/Destinos/DestinoTuristicoAppService.cs
@@ -0,0 +1,68 @@
+﻿using ExploraYa1.Destinosturisticos;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+using Xunit;
+using Volo.Abp.Testing;
+using Volo.Abp.Autofac;
+
+
+namespace ExploraYa1.Destinos
+{
+    public abstract class DestinoTuristicoAppService_tests<TStartupModule> : ExploraYa1ApplicationTestBase<TStartupModule>
+        where TStartupModule : IAbpModule
+
+        
+    {
+        private readonly IDestinosAppService _destinosAppService;
+   
+        protected DestinoTuristicoAppService_tests()
+        {
+            _destinosAppService = GetRequiredService<IDestinosAppService>();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnCreatedDestinosDto()         
+        {
+            // Arrange
+            var crearDestinoDTO = new CrearActualizarDestinoDTO
+            {
+                Nombre = "Parque Nacional",
+                Latitud = 34,
+                Longuitud = -118,
+                Poblacion = 500,
+                CalificacionGeneral = 5,
+                ImagenUrl = "asdasdasd",
+                RegionId = new Guid("22222222 - 2222 - 2222 - 2222 - 222222222222") // Asegúrate de usar un GUID válido
+            };
+            
+            // Act
+            var createdDestino = await _destinosAppService.CreateAsync(crearDestinoDTO);
+            
+            
+            
+            // Assert
+            createdDestino.ShouldNotBeNull();
+            createdDestino.Id.ShouldNotBe(Guid.Empty);
+            createdDestino.Nombre.ShouldBe(crearDestinoDTO.Nombre);
+            createdDestino.Poblacion.ShouldBe(crearDestinoDTO.Poblacion);
+            createdDestino.CalificacionGeneral.ShouldBe(crearDestinoDTO.CalificacionGeneral);
+            createdDestino.ImagenUrl.ShouldBe(crearDestinoDTO.ImagenUrl);
+            createdDestino.Latitud.ShouldBe(crearDestinoDTO.Latitud);
+            createdDestino.Longuitud.ShouldBe(crearDestinoDTO.Longuitud);
+            createdDestino.RegionId.ShouldBe(crearDestinoDTO.RegionId);
+        }
+
+    }
+
+
+
+
+
+
+}
---
+++ test/ExploraYa1.EntityFrameworkCore.Tests/Destinos/EFCoreDestinoTuristicoAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Volo.Abp.Testing;
+using System.Collections.Generic;
+using ExploraYa1.EntityFrameworkCore;
+
+
+namespace ExploraYa1.Destinos
+{
+    [Collection(ExploraYa1TestConsts.CollectionDefinitionName)]
+
+
+    public class EFCoreDestinoTuristicoAppService_tests : DestinoTuristicoAppService_tests<ExploraYa1EntityFrameworkCoreTestModule>
+    {
+    }
+}
