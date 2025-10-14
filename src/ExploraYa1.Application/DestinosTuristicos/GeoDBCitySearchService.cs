824214038494031b1d22c887b387844c84eb12fe
Felipe <Felipe@FELIPE-NAVE12>
2025-10-14 17:26:09 -0300

    a

---
+++ src/ExploraYa1.Application/DestinosTuristicos/GeoDBCitySearchService.cs
@@ -0,0 +1,21 @@
+﻿using ExploraYa1.Destinos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ExploraYa1.DestinosTuristicos
+//{
+//    public class GeoDbCitySearchService : ICitySearchService
+//    {
+//        public Task<CitySearchResultDto> SearchCitiesAsync(CitySearchRequestDto request)
+//        {
+            //throw new NotImplementedException();
+        
+        
+        
+//        }
+//    }
+//}
+
