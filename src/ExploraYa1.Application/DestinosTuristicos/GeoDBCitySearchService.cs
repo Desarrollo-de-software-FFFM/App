358b42b8c4dfe42b229b4eed639e55aca940979e
Felipe <Felipe@FELIPE-NAVE12>
2025-10-15 20:11:15 -0300

    v

--- src/ExploraYa1.Application/DestinosTuristicos/GeoDBCitySearchService.cs
+++ src/ExploraYa1.Application/DestinosTuristicos/GeoDBCitySearchService.cs
@@ -69,8 +69,8 @@ namespace ExploraYa1.DestinosTuristicos
 
         private class GeoDbCity
         {
-            public string City { get; set; }
-            public string Country { get; set; }
+            public string? City { get; set; }
+            public string? Country { get; set; }
             //public double Latitude { get; set; }
             //public double Longitude { get; set; }
         }
