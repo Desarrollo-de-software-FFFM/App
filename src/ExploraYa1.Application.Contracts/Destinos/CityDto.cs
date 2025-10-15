e3e2bd6ba5d5c1b00882b4abb4574bdc049398cc
Felipe <Felipe@FELIPE-NAVE12>
2025-10-15 20:18:00 -0300

    v

--- src/ExploraYa1.Application.Contracts/Destinos/CityDto.cs
+++ src/ExploraYa1.Application.Contracts/Destinos/CityDto.cs
@@ -4,8 +4,9 @@
     {
         public string  Name { get; set; }
         public string Country { get; set; }
-       // public double Latitude { get; set; }
-       // public double Longitude { get; set; }
+        public double Latitude { get; set; }
+       
+        public double Longitude { get; set; }
 
     }
 }
