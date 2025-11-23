88aa5de3c0888e4bcd314a6105cd3be3972e50ab
Franc <francisco2do1994@gmail.com>
2025-11-22 22:57:31 -0300

    Frontend con el buscar ciudades andando

--- src/ExploraYa1.Application.Contracts/Destinos/CitySearchRequestDto.cs
+++ src/ExploraYa1.Application.Contracts/Destinos/CitySearchRequestDto.cs
@@ -1,7 +1,11 @@
-﻿namespace ExploraYa1.Destinos
+﻿using Volo.Abp.Application.Dtos;
+
+namespace ExploraYa1.Destinos
 {
-    public class CitySearchRequestDto
+    // 👇 AL AGREGAR ESTA HERENCIA, GANAS PAGINACIÓN AUTOMÁTICA
+    public class CitySearchRequestDto : PagedAndSortedResultRequestDto
     {
         public string PartialName { get; set; }
+        public string Country { get; set; }
     }
 }
