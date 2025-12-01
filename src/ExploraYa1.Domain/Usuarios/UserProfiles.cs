b614763d0ce26650fc3ecbd513f884dc20230db2
Felipe <Felipe@FELIPE-NAVE12>
2025-12-01 20:47:15 -0300

    a

---
+++ src/ExploraYa1.Domain/Usuarios/UserProfiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace ExploraYa1.UserProfiles
+{
+    public class UserProfile : FullAuditedAggregateRoot<Guid>
+    {
+        public Guid UserId { get; set; }   // Id del usuario de ABP.Identity
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string FotoUrl { get; set; }
+
+        protected UserProfile() { }
+
+        public UserProfile(Guid id, Guid userId, string nombre, string apellido, string telefono, string fotoUrl)
+            : base(id)
+        {
+            UserId = userId;
+            Nombre = nombre;
+            Apellido = apellido;
+            Telefono = telefono;
+            FotoUrl = fotoUrl;
+        }
+    }
+}
