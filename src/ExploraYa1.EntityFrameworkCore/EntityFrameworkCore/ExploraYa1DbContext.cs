a2d0c84ea11cd4da5b9cb07b04b1264a76c61d2a
felip <felip@DESKTOP-MC6RCVB>
2025-12-05 16:09:39 -0300

    a

--- src/ExploraYa1.EntityFrameworkCore/EntityFrameworkCore/ExploraYa1DbContext.cs
+++ src/ExploraYa1.EntityFrameworkCore/EntityFrameworkCore/ExploraYa1DbContext.cs
@@ -209,7 +209,7 @@ public class ExploraYa1DbContext :
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("Server=FELIPE-NAVE12;Database=ExploraYa1;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\FELIPE-NAVE13;Database=ExploraYa1;Trusted_Connection=True;TrustServerCertificate=True;");
         }
     }
     
