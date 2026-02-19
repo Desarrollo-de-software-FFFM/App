d58324185ef15f3fea53c277d6fb70b419437b86
felipemartinez125407-hub <felipemartinez125407@gmail.com>
2026-02-18 22:01:24 -0300

    Revert "Revert "Revert "Feature/20 destinosturisticosfeli(destino a stg)"""

--- src/ExploraYa1.Application.Contracts/Destinos/CityDto.cs
+++ src/ExploraYa1.Application.Contracts/Destinos/CityDto.cs
@@ -2,15 +2,13 @@
 {
     public class CityDto
     {
-        public int Id { get; set; } 
-        public string Name { get; set; } = string.Empty;
-        public string Country { get; set; } = string.Empty;
-
-        public string? Region { get; set; }          // NUEVO
-        public int? Population { get; set; }         // NUEVO
-
+        public string  Name { get; set; }
+        public string Country { get; set; }
+       
+      
         public double Latitude { get; set; }
+       
         public double Longitude { get; set; }
-    }
 
+    }
 }
