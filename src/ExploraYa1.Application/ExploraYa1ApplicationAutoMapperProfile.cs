1453baa3dbde0c01e3cfe8703d126eec4c4ace04
Felipe <Felipe@FELIPE-NAVE12>
2026-02-19 22:30:09 -0300

    a

--- src/ExploraYa1.Application/ExploraYa1ApplicationAutoMapperProfile.cs
+++ src/ExploraYa1.Application/ExploraYa1ApplicationAutoMapperProfile.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using ExploraYa1.Destinos;
 using ExploraYa1.DestinosTuristicos;
 using ExploraYa1.Notificaciones;
+using Volo.Abp.Identity;
 
 namespace ExploraYa1;
 
@@ -18,6 +19,9 @@ public class ExploraYa1ApplicationAutoMapperProfile : Profile
 
         CreateMap<CalificacionDestino, CalificacionDto>()
             .ForMember(d => d.DestinoTuristicoId, opt => opt.MapFrom(s => s.DestinoTuristicoId));
+
+        
+
 
         CreateMap<Notificacion, NotificacionDTO>();
 
