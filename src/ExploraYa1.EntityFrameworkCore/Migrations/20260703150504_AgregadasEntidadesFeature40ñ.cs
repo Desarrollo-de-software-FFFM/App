dc73fa571af6ae12325fa9504106de95f1a9fe68
Felipe <Felipe@FELIPE-NAVE12>
2026-07-03 12:10:57 -0300

    a

--- src/ExploraYa1.EntityFrameworkCore/Migrations/20260703150504_AgregadasEntidadesFeature40ñ.cs
+++ src/ExploraYa1.EntityFrameworkCore/Migrations/20260703150504_AgregadasEntidadesFeature40ñ.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace ExploraYa1.Migrations
 {
     /// <inheritdoc />
-    public partial class AgregadasEntidadesFeature25 : Migration
+    public partial class AgregadasEntidadesFeature40ñ : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // VACIADO: No ejecuta comandos para evitar conflictos con lo que ya creó STG
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // VACIADO
+
         }
     }
 }
