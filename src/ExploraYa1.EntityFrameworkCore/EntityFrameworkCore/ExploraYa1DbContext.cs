ecbeca8290c94cf80187e23e630a3a9186a64ce0
Felipe <Felipe@FELIPE-NAVE12>
2026-07-03 10:26:33 -0300

    a

--- src/ExploraYa1.EntityFrameworkCore/EntityFrameworkCore/ExploraYa1DbContext.cs
+++ src/ExploraYa1.EntityFrameworkCore/EntityFrameworkCore/ExploraYa1DbContext.cs
@@ -250,7 +250,7 @@ public class ExploraYa1DbContext :
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\FELIPE-NAVE12;Database=ExploraYa1;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=FELIPE-NAVE12;Database=ExploraYa1;Trusted_Connection=True;TrustServerCertificate=True;");
         }
     }
 
